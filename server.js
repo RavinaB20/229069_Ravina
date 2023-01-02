c2743a0b5c345bacb5c9395dd35225cd9c8277c1
work-abhisheksingh <dac@dypm.org>
2023-01-02 18:56:59 +0530

    Adding all course related files

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+var app = express();
+var bodyparser = require ('body-parser');
+
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.get('/',function(req,res){
+    res.sendFile("public/index.html",{root:__dirname})
+});
+
+app.get('/schedule',function(req,res){
+    res.sendFile("public/schedule.html",{root:__dirname})
+});
+
+app.listen(3000);
+
+console.log("Server Running on port 3000");
